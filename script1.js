e9376c8f5241ab6089ab53f9ffcda4346dd4cbcc
rajat-16rajput <67589444+rajat-16rajput@users.noreply.github.com>
2021-10-28 13:04:45 +0530

    first commit

---
+++ script1.js
@@ -0,0 +1,9 @@
+function initMap()
+{
+   map = new google.maps.Map(document.getElementById('map'), {
+  center: {lat: 18.99279688393996, lng: 73.2777118652341},
+  zoom: 15,
+  mapId: '97412f0fd4f03712'
+});
+}
+
